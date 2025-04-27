ed63a1eee30dfed11310dafc1ff05e8710928830
eshal-irfan10 <developereshal8@gmail.com>
2025-04-27 15:08:10 +0500

    hunain

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
 });
 
 // Server Listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || "https://backendtask-psi.vercel.app";
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
