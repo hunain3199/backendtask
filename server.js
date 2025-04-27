0bcbb0c218b6676d31310b072b25b625c48c0a34
eshal-irfan10 <developereshal8@gmail.com>
2025-04-27 15:00:15 +0500

    hunain

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS Configuration to allow specific origins
 const corsOptions = {
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: ['http://localhost:5174', 'http://localhost:5173','https://frontendtask-ten-virid.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as per your needs
   credentials: true, // If you need to allow cookies or other credentials
 };
