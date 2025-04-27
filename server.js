a65bd95d1e04c372178908eeb3d2c9687aae1d92
eshal-irfan10 <developereshal8@gmail.com>
2025-04-27 14:59:07 +0500

    eshall

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 
-
 dotenv.config();
 
 const app = express();
 
+// CORS Configuration to allow specific origins
+const corsOptions = {
+  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as per your needs
+  credentials: true, // If you need to allow cookies or other credentials
+};
+
 // Middlewares
-app.use(cors());
+app.use(cors(corsOptions)); // Apply CORS middleware with the specific options
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
+
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
